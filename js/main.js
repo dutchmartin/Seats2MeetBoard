d3a232e64a517c73d6bd7f3f6d94a1bfa5b6543d
dutchmartin <martijn.groeneveldt@gmail.com>
2019-07-02 16:23:50 +0200

    do it

--- js/main.js
+++ js/main.js
@@ -9,45 +9,89 @@ const LocationId = 85;
 
 const contentCardsHolder = document.getElementById("content-cards-holder");
 
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append("token", "398140257");
+
+
+
 function updateCards() {
-
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append("token", "398140257");
-
-    const url = 'http://staging.seats2meet.com/api/event/public/location/' + LocationId;
-    fetch(url, {
+    fetch('http://staging.seats2meet.com/api/meetingspace/location/' + LocationId, {
         headers: headers,
         method: 'GET',
         cache: 'no-cache',
     })
         .then(response => response.json())
         .then(
-        function(data){
+            function(data){
+                let rooms = [];
+                for (let item of data.Results) {
+                    rooms.push({
+                        'Name': item.Name,
+                        'Title': 'leeg',
+                        'CompanyName': 'Onbekend',
+                        'StartTime': "0",
+                        'EndTime': "0",
+                    })
+                }
+                return rooms;
+            }).then(function (rooms) {
+        const url = 'http://staging.seats2meet.com/api/event/public/location/' + LocationId;
+        fetch(url, {
+            headers: headers,
+            method: 'GET',
+            cache: 'no-cache',
+        })
+            .then(response => response.json())
+            .then(
+                function(data){
+                    for(let meeting of data){
+                        console.log(meeting);
+                        for(let meetingspace of meeting.Meetingspaces){
+                            // search for meetingspace.
+                            console.log(meetingspace);
+                            let index = rooms.findIndex(room => meetingspace === room.Name);
+                            if(index >= 0) {
+                                rooms[index].Title = meeting.Name;
+                                rooms[index].CompanyName = meeting.CompanyName;
+                                rooms[index].StartTime = meeting.StartTime;
+                                rooms[index].EndTime = meeting.EndTime;
+                            }
+                            else {
+                                console.log('did not find' + meetingspace)
+                            }
+                        }
+                    }
 
-        contentCardsHolder.innerHTML = "";
-        for(let item of data) {
-            contentCardsHolder.innerHTML +=
-                "            <div class=\"col\" id=\"roomtile\">" +
-                "                <div id=\"roomtilecurrent\">" +
-                "                    <u class=\"roomtitle\">"+ item.Meetingspaces[0] +"</u>" +
-                "                    <br/>" +
-                "                    <span class=\"coursetitle\">"+ item.Name +"</span>" +
-                "                    <br/>" +
-                "                    <span class=\"companytitle\">" + item.CompanyName + "</span>" +
-                "                    <br/>" +
-                "                    <span class=\"bookedtime\">"+ moment(item.StartTime).format("H:mm") +"-"+ moment(item.EndTime).format("H:mm") +"</span>" +
-                "                </div>" +
-                // "                <div id=\"roomtilenext\">\n" +
-                // "                    <span class=\"next-company\">00:00 - volgend bedrijf</span>" +
-                // "                </div>" +
-                "            </div>"
-        }
-    })
-        .catch(function(error) {
-            console.log('There has been a problem with your fetch operation: ', error.message);
-        });
-    console.log("tick..");
+                    rooms.sort((a, b) => (a.Name > b.Name) ? 1 : -1);
+                    contentCardsHolder.innerHTML = "";
+                    for(let item of rooms) {
+                        console.log(item);
+                        contentCardsHolder.innerHTML +=
+                            "            <div class=\"col\" id=\"roomtile\">" +
+                            "                <div id=\"roomtilecurrent\">" +
+                            "                    <u class=\"roomtitle\">"+ item.Name +"</u>" +
+                            "                    <br/>" +
+                            "                    <span class=\"coursetitle\">"+ item.Title +"</span>" +
+                            "                    <br/>" +
+                            "                    <span class=\"companytitle\">" + item.CompanyName + "</span>" +
+                            "                    <br/>" +
+                            "                    <span class=\"bookedtime\">"+ moment(item.StartTime).format("H:mm") +"-"+ moment(item.EndTime).format("H:mm") +"</span>" +
+                            "                </div>" +
+                            // "                <div id=\"roomtilenext\">\n" +
+                            // "                    <span class=\"next-company\">00:00 - volgend bedrijf</span>" +
+                            // "                </div>" +
+                            "            </div>"
+                    }
+                })
+            .catch(function(error) {
+                console.log('There has been a problem with your fetch operation: ', error.message);
+            });
+
+    }).catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ', error.message);
+    });
+
     setTimeout(updateCards, 100000);
 }
 
