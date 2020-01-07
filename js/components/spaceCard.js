3d6a249f39b35af70368dac0599b070286291b22
Dennis Vergeer <dennis@seats2meet.com>
2020-01-07 16:47:34 +0100

    Card update

--- js/components/spaceCard.js
+++ js/components/spaceCard.js
@@ -34,27 +34,25 @@ Vue.component('space-card', {
             let currentMinutes =  this.$options.filters.timeToMinutes(this.currentTime);
             let currentMeeting = null;
 
-            for(let i in this.meetings) {
-                let _meeting = this.meetings[i];
-                let _startMinutes = _meeting.StartMinutes - 30;
-                let _endMinutes = _meeting.EndMinutes - 30;
-
-                if((currentMinutes >= _startMinutes && currentMinutes <= _endMinutes)) {
-                    currentMeeting = _meeting;
-                    break;
+            if(this.meetings.length === 1) {
+                if(currentMinutes <= this.meetings[0].EndMinutes) {
+                    currentMeeting = this.meetings[0];
                 }
+            } else {
+                for(let i in this.meetings) {
+                    let _meeting = this.meetings[i];
+                    let _startMinutes = _meeting.StartMinutes - 30;
+                    let _endMinutes = _meeting.EndMinutes - 30;
 
-                // Return next meeting when
-                if(Number(i + 1) < this.meetings.length) {
-                    // console.info(currentMinutes, this.meetings[Number(i + 1)].StartMinutes);
-                    // if(currentMinutes >= _endMinutes && currentMinutes <= this.meetings[Number(i + 1)].StartMinutes) {
-                        currentMeeting = this.meetings[i + 1];
+                    if((currentMinutes >= _startMinutes && currentMinutes <= _endMinutes)) {
+                        currentMeeting = _meeting;
                         break;
-                    // }
-                }
-                else if(this.meetings.length === 1) {
-                    if(currentMinutes <= this.meetings[i].StartMinutes) {
-                        currentMeeting = this.meetings[i];
+                    } else if(Number(i + 1) < this.meetings.length) {
+                        if(currentMinutes <= this.meetings[Number(i + 1)].StartMinutes) {
+                            currentMeeting = this.meetings[Number(i + 1)];
+                            console.info(currentMeeting.Name)
+                            break;
+                        }
                     }
                 }
             }
